806f7a09d95a0ffb77c6847e0fac1f502837717b
Piers Shepperson <piers.shepperson@btinternet.com>
2018-03-11 18:44:18 +0000

    Example transcompliation.

---
+++ transpiler/data/exampleCompile.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+contract Base {
+    uint xBase
+}
+
+contract Derived is Base{
+  uint public pubSV;
+  uint internal intSV;
+  uint private priSV;
+
+  function extF(uint _value) external  {
+    pubSV = _value;
+  }
+
+  function pubF(uint _value) public  returns (uint) {
+    pubSV = _value;
+    return pubSV;
+  }
+
+  function intF(uint _value) internal  returns (uint) {
+    intSV = _value;
+    return intSV;
+  }
+
+  function priF(uint _value) private  returns (uint) {
+    priSV = _value;
+    return priSV;
+  }
+}
