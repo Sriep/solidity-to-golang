2b41e09b0f4eb2358648c11be8152b152e86f339
Piers Shepperson <piers.shepperson@btinternet.com>
2018-03-16 22:56:54 +0000

    Setup testing framwork. Run test/runtest.bar to perform unit tests.

--- test/test-sols/simplestore_test.go
+++ test/test-sols/simplestore_test.go
@@ -7,12 +7,12 @@ import "math/big"
 func TestSetGet(t *testing.T) {
 
 myStore := NewSimpleStore()
-for  i  := 0; i < 10; i++ {
-bigI := big.NewInt(int64(i))
-myStore.Set(bigI)
-rtv := myStore.Get()
-if rtv != bigI {
-t.Fatalf("Set %d but got %d", bigI, rtv)
-}
-}
+    for  i  := 0; i < 10; i++ {
+        bigI := big.NewInt(int64(i))
+        myStore.Set(bigI)
+        rtv := myStore.Get()
+        if rtv != bigI {
+            t.Fatalf("Set %d but got %d", bigI, rtv)
+        }
+    }
 };
